9f3b42bff1fa5ff1513ae30a0b961ea65ba68610
Muhammad Hasan Alam <156111320+Hasanalam45@users.noreply.github.com>
2026-01-09 15:20:31 +0500

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,10 +1,9 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+{
+  "rewrites": [
+    {
+      "source": "/(.*)",
+      "destination": "/index.html"
+    }
+  ]
+}
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react(),
-    tailwindcss()
-  ],
-})
